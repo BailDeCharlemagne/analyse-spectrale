41c20dcd78be3b0a158a40cf5e479a72cbd9b152
engly_monyrath@yahoo.fr <engly_monyrath@yahoo.fr>
2016-11-14 01:59:23 +0100

    added imagesc feature + 3 max values de la matrix

--- pisarenko/flute_pisarenko_intervalles.m
+++ pisarenko/flute_pisarenko_intervalles.m
@@ -20,19 +20,7 @@ f_subsampling = 4;
 %figure;
 %plot(t, flute);
 %title('Signal pur de la flute - non bruité'); 
-
-%%% Add noise to signal 
-%
-%Eb_No = 2; %en dB 
-%Eb = abs((norm(flute)^2))/length(flute);
-%No = Eb_No*log(-Eb/10); 
-%variance = No/2; %Revoir avec Telecom
-%bruit = randn(length(flute),1)*variance; 
-%noisy_flute = bruit + flute;
-%%figure; 
-%%plot(t, noisy_flute) 
-%%title('Signal bruité de la flute');  
-
+  
 
  
 ###########################################################################
@@ -40,7 +28,7 @@ f_subsampling = 4;
 ###########################################################################
 
 close all; 
-order = 10; 
+order = 1; 
 parameter = 1 ; % 0 donne la DSP, 1 le spectre de raies, 2 l'enveloppe interpolée
 plot_or_not_plot = 0;
 
@@ -55,48 +43,60 @@ for i=1:nbIntervals
   flute_ech(i,:) = flute_t((i-1)*interval_ech+1:i*interval_ech); 
 end; 
 
-%% Pisarenko sur ces petits signaux 
+% Pisarenko sur ces petits signaux 
+
+% Matrice pour construire le temps fréquence amplitude
+M_full = zeros(nbIntervals, fs/8);
+
+for i=1:nbIntervals
+  # Pisarenko 1 
+  [ff_pisa, mydsp_pisa] = pisarenko_real(flute_ech(i,:), order, fs, parameter, plot_or_not_plot); % donne spectre de raies
+  mydsp_pisa1 = zeros(1,mydsp_pisa);
+  for k=1:length(mydsp_pisa) 
+    if (mydsp_pisa(k) > 10^(-7))
+      mydsp_pisa1(k) = mydsp_pisa(k);
+    else mydsp_pisa1(k) = 0;
+    end; 
+  end; 
+  M_full(i,:) = transpose(mydsp_pisa1(1:fs/8)); 
+  str = ['done ', num2str(i)];
+  disp(str)
+end;
+
+figure; 
+imagesc(transpose(M_full));
+
+% TO DO 
+A = [1 2 3 4 8 5 2 9 ; 5 6 4 7 9 1 1 2]
+[maxvals, maxindices] = sort(A, 2, 'descend');
+maxvals = maxvals(:, 1:4)
+maxindices = maxindices(:, 1:4)
+%
+
+
+%% Display pour obtenir slide 17
+%
+%figure;
+%mesh(M_full); 
+%xlabel('Fréquence'); ylabel('Intervalle n°'); zlabel('Amplitude de la DSP'); 
+%rotate3d on; 
 
 %% Matrice pour construire le temps fréquence amplitude
-%M_full = zeros(nbIntervals, fs/8);
+%M_relevant = zeros(nbIntervals, fs/8);
 %
 %for i=1:nbIntervals
 %  # Pisarenko 1 
 %  [ff_pisa, mydsp_pisa] = pisarenko_real(flute_ech(i,:), order, fs, parameter, plot_or_not_plot); % donne spectre de raies
-%  mydsp_pisa1 = zeros(1,mydsp_pisa);
-%  for k=1:length(mydsp_pisa) 
-%    if (mydsp_pisa(k) > 10^(-7))
-%      mydsp_pisa1(k) = mydsp_pisa(k);
-%    end; 
-%  end; 
-%  M_full(i,:) = transpose(mydsp_pisa1(1:fs/8)); 
+%  mydsp_pisa(mydsp_pisa < 10^-9) = NaN;
+%  M_relevant(i,:) = transpose(mydsp_pisa(1:fs/8)); 
 %  str = ['done ', num2str(i)];
 %  disp(str)
 %end;
-
+%
 %% Display
 %
 %figure;
 %mesh(M_relevant); 
 %xlabel('Fréquence'); ylabel('Intervalle n°'); zlabel('Amplitude de la DSP'); 
 %rotate3d on; 
-
-% Matrice pour construire le temps fréquence amplitude
-M_relevant = zeros(nbIntervals, fs/8);
-
-for i=1:nbIntervals
-  # Pisarenko 1 
-  [ff_pisa, mydsp_pisa] = pisarenko_real(flute_ech(i,:), order, fs, parameter, plot_or_not_plot); % donne spectre de raies
-  mydsp_pisa(mydsp_pisa < 10^-9) = NaN;
-  M_relevant(i,:) = transpose(mydsp_pisa(1:fs/8)); 
-  str = ['done ', num2str(i)];
-  disp(str)
-end;
-
-% Display
-
-figure;
-mesh(M_relevant); 
-xlabel('Fréquence'); ylabel('Intervalle n°'); zlabel('Amplitude de la DSP'); 
-rotate3d on; 
 
